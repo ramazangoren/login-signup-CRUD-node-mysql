ca8d0667d0a5788d7d82bdf35d0f3d7898cb6aac
Ramazan  Gören <ramazangoren@Ramazan.local>
2021-10-10 21:03:36 +0300

    delete some unnecessary  data

--- routes/signup.js
+++ routes/signup.js
@@ -2,17 +2,8 @@ const express = require('express');
 const save = require('../controllers/save')
 const router = express.Router();
 
-
-
 router.post('/sign/register', save.register)
-
-
 router.post('/password/reset', save.update)
-
-
-
-
-
 
 
 
